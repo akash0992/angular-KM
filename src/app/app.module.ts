e0745cbfeeb3b7ce9fe0aa7fc1ef5cbc3158c0af
Akash Yadav <akash.yadav@tothenew.com>
2017-07-22 01:56:36 +0530

    added input-aliasing, breaking of component at level-3, ng-model

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,26 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { MainViewComponent } from './components/main-view/main-view.component';
+import { TodoListComponent } from './components/todo-list/todo-list.component';
+import { TodoDetailsComponent } from './components/todo-details/todo-details.component';
+import { TodoComponent } from './components/todo/todo.component';
+import {TodoService} from './services/todo.service';
+import { CounterComponent } from './components/counter/counter.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     MainViewComponent,
+    TodoListComponent,
+    TodoDetailsComponent,
+    TodoComponent,
+    CounterComponent,
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [TodoService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
